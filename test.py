00d11a87b0cc479889310f5ef464f3e38874d79c
LauraCruciani <laura.cruciani@mail.polimi.it>
2023-12-20 16:36:23 +0100

    .

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import os
 import cv2
 import torch.backends.cudnn as cudnn
 import numpy as np
-import torchvision 
+from tqdm import tqdm
 import numpy as np
 import csv
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, roc_auc_score, jaccard_score
@@ -24,14 +24,16 @@ checkpoint = torch.load('best_model.pth')
 model.load_state_dict(checkpoint['net'])
 #load_checkpoint(torch.load("best_model.pth"), model)
 print('\033[0;33mModel Loaded!\033[0m')
+metric_values=[]
+
 if not os.path.exists(folder):
     print(f"The folder'{folder}' doesn't exist.")
-    
+
 else: 
     files = os.listdir(folder) 
     images = [file for file in files if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))]
 
-    for immagine in images:
+    for immagine in tqdm(images):
         path_immagine = os.path.join((folder), immagine)
         img = cv2.imread(path_immagine)
         img=cv2.resize(img, (240,160))
@@ -45,28 +47,22 @@ else:
         target = cv2.imread(path_mask,0)
         target=cv2.resize(target, (240,160))
         target=np.asarray(target)
-        # target = target.transpose((2, 0, 1))  
-        # target = target / 255.0
-        # target = torch.from_numpy(target).uint8().unsqueeze(0).to('cuda')
+
         with torch.no_grad():
             start=time.time()
             preds = torch.sigmoid(model(img))
             preds = (preds > 0.5).float()
             end=time.time()-start
-            print(end)
-            print(preds.size(), target.shape)
             preds = preds.squeeze().cpu().numpy() * 255
             preds=preds.astype(np.uint8)
             concatenated_img = np.concatenate((target, preds), axis=1)
             cv2.imwrite(f"test/{name}", concatenated_img)
-
-            print(preds.max(), target.max())
             target_flat = target.flatten()
             preds_flat = preds.flatten()
-
             target_flat=(target_flat/255).astype(np.uint8)
             preds_flat=(preds_flat/255).astype(np.uint8)
 
+            ## Metrics
             accuracy = accuracy_score(target_flat, preds_flat)
             recall = recall_score(target_flat, preds_flat)
             precision = precision_score(target_flat, preds_flat)
@@ -75,8 +71,6 @@ else:
             jaccard = jaccard_score(target_flat, preds_flat)
             with open(csv_filename, 'a', newline='') as csvfile:
                 writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-                # Scrive i risultati delle metriche nel file CSV
                 writer.writerow({
                     'Image': immagine,
                     'Accuracy': accuracy,
@@ -88,3 +82,10 @@ else:
                     'Time': end
                 })
 
+                metric_values.append([accuracy, recall, precision, f1, roc_auc, jaccard, end])
+    metric_values.append([accuracy, recall, precision, f1, roc_auc, jaccard, end])
+    column_means = np.mean(metric_values, axis=0)
+    with open(csv_filename, 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([]) 
+        writer.writerow(['Mean'] + list(column_means))
